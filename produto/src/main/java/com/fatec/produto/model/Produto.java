a9242d39d6eae7b6152879b63f684f8364b2208e
ffvprogweb <disclabes@gmail.com>
2023-09-29 17:31:05 -0300

    versão inicial

---
+++ produto/src/main/java/com/fatec/produto/model/Produto.java
@@ -0,0 +1,58 @@
+package com.fatec.produto.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Produto {
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+
+	private Long id;
+	private String descricao;
+	private String categoria;
+	private double custo;
+	private int quantidaNoEstoque;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		if(descricao == null) {
+			throw new IllegalArgumentException ("A descrição não pode ser branco");
+		} else {
+			if (descricao.isBlank()) {
+				throw new IllegalArgumentException ("A descrição não pode ser branco");
+			} else {
+				this.descricao = descricao;
+			}
+		}
+		
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public double getCusto() {
+		return custo;
+	}
+	public void setCusto(double custo) {
+		this.custo = custo;
+	}
+	public int getQuantidaNoEstoque() {
+		return quantidaNoEstoque;
+	}
+	public void setQuantidaNoEstoque(int quantidaNoEstoque) {
+		this.quantidaNoEstoque = quantidaNoEstoque;
+	}
+	
+}
